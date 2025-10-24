309c54075151d54d7f51ddc42703cd0961657fde
uzmakiran15 <uzmakiran15@gmail.com>
2025-10-24 11:32:42 +0500

    my fluter porject

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(children: [Text('Hello World!'), Text("hi another one")]),
+        ),
+      ),
+    );
+  }
+}
